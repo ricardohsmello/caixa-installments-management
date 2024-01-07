74da6bcb2b86cb19fa0f5d2579d05100e464e0cf
Ricardo <ricardohsmello@gmail.com>
2024-01-06 21:10:00 -0300

    Installment repository

--- src/main/kotlin/br/com/ricas/caixa/infrastructure/InstallmentsRepository.kt
+++
@@ -1,6 +0,0 @@
-package br.com.ricas.caixa.infrastructure
-
-import br.com.ricas.caixa.domain.entity.InstallmentDocument
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface InstallmentsRepository : MongoRepository<InstallmentDocument, String>
