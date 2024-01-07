c04d959786723003c4e78dab10ffb96ec50cef7e
Ricardo <ricardohsmello@gmail.com>
2024-01-06 21:10:26 -0300

    Added RicasLog annotation and usage

--- src/main/kotlin/br/com/ricas/caixa/application/api/rest/InstallmentsController.kt
+++ src/main/kotlin/br/com/ricas/caixa/application/api/rest/InstallmentsController.kt
@@ -3,6 +3,8 @@ package br.com.ricas.caixa.application.api.rest
 import br.com.ricas.caixa.application.api.request.InstallmentRequest
 import br.com.ricas.caixa.domain.entity.InstallmentDocument
 import br.com.ricas.caixa.domain.service.InstallmentsService
+import br.com.ricas.caixa.infrastructure.logging.RicasLog
+import br.com.ricas.caixa.infrastructure.logging.logger
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,12 +12,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/installments")
+@RicasLog
 class InstallmentsController(
     private val installmentsService: InstallmentsService
 ) {
-
     @PostMapping("/create")
     fun create(@RequestBody installmentRequest: InstallmentRequest) {
+        logger().info(
+            "Starting create installment with: $installmentRequest"
+        )
+
         installmentsService.create(InstallmentDocument.toDomain(installmentRequest))
     }
 }
---
+++ src/main/kotlin/br/com/ricas/caixa/infrastructure/logging/RicasLog.kt
@@ -0,0 +1,11 @@
+package br.com.ricas.caixa.infrastructure.logging
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class RicasLog
+
+inline fun <reified T> T.logger(): Logger {
+    return LoggerFactory.getLogger(T::class.java)
+}
