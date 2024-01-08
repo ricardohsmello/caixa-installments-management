6700d3d3a8907dc6725af310fcd6d4ec321c43c6
Ricardo <ricardohsmello@gmail.com>
2024-01-08 19:50:20 -0300

    refactor application

--- src/main/kotlin/br/com/ricas/caixa/application/config/MongoDB.kt
+++ src/main/kotlin/br/com/ricas/caixa/application/config/MongoDB.kt
@@ -6,18 +6,4 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class MongoDB {
 
-    @Value("\${username}")
-    lateinit var username: String
-
-    @Value("\${password}")
-    lateinit var password: String
-
-    @Value("\${cluster}")
-    lateinit var cluster: String
-
-    @Value("\${database}")
-    lateinit var database: String
-
-
-
 }
