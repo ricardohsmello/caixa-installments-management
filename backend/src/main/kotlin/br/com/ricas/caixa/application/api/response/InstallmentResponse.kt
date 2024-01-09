bc1386c9ff21c7c0bf6a1ccf9c7352573d948980
Ricardo <ricardohsmello@gmail.com>
2024-01-09 02:11:45 -0300

    refactor application

---
+++ backend/src/main/kotlin/br/com/ricas/caixa/application/api/response/InstallmentResponse.kt
@@ -0,0 +1,17 @@
+package br.com.ricas.caixa.application.api.response
+
+import java.util.*
+
+data class InstallmentResponse(
+    val nroContrato: String,
+    val dueDate: Date,
+    val paid: Boolean,
+    val amount: Double,
+    val interest: Double,
+    val insurance: Double,
+    val fees: Int,
+    val outstandingBalance: Double,
+    val monetaryCorrection: Double,
+    val amortization: Double,
+    val isContribution: Boolean
+)
