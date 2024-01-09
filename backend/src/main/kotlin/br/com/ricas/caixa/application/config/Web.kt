1894eed2352b45dd9249a4f782c70164672f94e8
Ricardo <ricardohsmello@gmail.com>
2024-01-09 02:11:52 -0300

    refactor application

--- backend/src/main/kotlin/br/com/ricas/caixa/application/config/Web.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/application/config/Web.kt
@@ -1,4 +1,20 @@
 package br.com.ricas.caixa.application.config
 
-class Web {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+@EnableWebMvc
+class Web : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/*")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"  )
+//            .allowedHeaders("header1", "header2", "header3")
+//            .exposedHeaders("header1", "header2")
+            .allowCredentials(false).maxAge(3600)
+    }
 }
