917dd94e277aca0b8a9b7848b167a4e849d08ae2
Ricardo <ricardohsmello@gmail.com>
2024-01-09 03:39:09 -0300

    added delete by id

--- backend/src/main/kotlin/br/com/ricas/caixa/domain/service/InstallmentsService.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/domain/service/InstallmentsService.kt
@@ -5,4 +5,5 @@ import br.com.ricas.caixa.domain.entity.InstallmentDocument
 interface InstallmentsService {
     fun create(installmentDocument: InstallmentDocument): InstallmentDocument
     fun findAll(): List<InstallmentDocument>
+    fun delete(id: String)
 }
