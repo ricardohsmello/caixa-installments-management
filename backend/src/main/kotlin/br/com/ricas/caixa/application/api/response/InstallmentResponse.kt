370adae366de1b837d0093bd828fac26e4ad0fcd
Ricardo <ricardohsmello@gmail.com>
2024-01-10 03:08:59 -0300

    Refactor

--- backend/src/main/kotlin/br/com/ricas/caixa/application/api/response/InstallmentResponse.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/application/api/response/InstallmentResponse.kt
@@ -3,16 +3,16 @@ package br.com.ricas.caixa.application.api.response
 import java.util.*
 
 data class InstallmentResponse(
-    val id: String,
-    val nroContrato: String,
-    val dueDate: Date,
-    val paid: Boolean,
-    val amount: Double,
-    val interest: Double,
-    val insurance: Double,
-    val fees: Int,
-    val outstandingBalance: Double,
-    val monetaryCorrection: Double,
-    val amortization: Double,
-    val isContribution: Boolean
+     val id: String?,
+    val nroContrato: String?,
+    val dueDate: Date?,
+    val paid: Boolean?,
+    val amount: Double?,
+    val interest: Double?,
+    val insurance: Double?,
+    val fees: Int?,
+    val outstandingBalance: Double?,
+    val monetaryCorrection: Double?,
+    val amortization: Double?,
+    val isContribution: Boolean?
 )
