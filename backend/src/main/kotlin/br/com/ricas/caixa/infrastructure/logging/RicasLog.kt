a2bdd725db568f597e680663c40760c9e4bcbeb0
Ricardo <ricardohsmello@gmail.com>
2024-01-16 22:57:40 -0300

    Refactor

--- backend/src/main/kotlin/br/com/ricas/caixa/infrastructure/logging/RicasLog.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/infrastructure/logging/RicasLog.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 annotation class RicasLog
-
 inline fun <reified T> T.logger(): Logger {
     return LoggerFactory.getLogger(T::class.java)
 }
