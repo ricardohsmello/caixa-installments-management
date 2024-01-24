28f2695360e620f8e3520c76b62dc7bcb5f6777e
Ricardo <ricardohsmello@gmail.com>
2024-01-23 23:13:52 -0300

    Refactor

--- backend/src/main/kotlin/br/com/ricas/caixa/domain/service/impl/InstallmentsServiceImpl.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/domain/service/impl/InstallmentsServiceImpl.kt
@@ -4,6 +4,7 @@ import br.com.ricas.caixa.domain.entity.InstallmentDocument
 import br.com.ricas.caixa.domain.service.InstallmentsService
 import br.com.ricas.caixa.infrastructure.repository.mongo.InstallmentsRepository
 import org.springframework.stereotype.Service
+
 @Service
 class InstallmentsServiceImpl(
     private val installmentsRepository: InstallmentsRepository
