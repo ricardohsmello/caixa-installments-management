101295fc04b41d0b2aa41406894680189b6b4ee9
Ricardo <ricardohsmello@gmail.com>
2024-01-30 23:44:37 -0300

    Refactor

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -18,6 +18,7 @@ java {
 repositories {
 	mavenCentral()
 }
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
