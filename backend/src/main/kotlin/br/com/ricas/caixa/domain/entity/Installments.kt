cec3d2b90b2d1b8537e34b6f062098a46f4c0de1
Ricardo <ricardohsmello@gmail.com>
2024-09-04 02:49:58 -0300

    adding  tesseras

--- backend/src/main/kotlin/br/com/ricas/caixa/domain/entity/Installments.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/domain/entity/Installments.kt
@@ -3,35 +3,24 @@ package br.com.ricas.caixa.domain.entity
 import br.com.ricas.caixa.application.api.request.InstallmentRequest
 import br.com.ricas.caixa.application.api.response.InstallmentResponse
 import br.com.ricas.caixa.infrastructure.entity.InstallmentsEntity
-import org.bson.codecs.pojo.annotations.BsonId
-import org.bson.codecs.pojo.annotations.BsonProperty
-import org.bson.types.ObjectId
 import java.util.Date
 
 data class Installments(
-    @BsonId
-    val id: ObjectId?,
-    @BsonProperty("nro_contrato")
     val nroContrato: String?,
-    @BsonProperty("due_date")
     val dueDate: Date?,
     val paid: Boolean?,
     val amount: Double?,
     val interest: Double?,
     val insurance: Double?,
     val fees: Int?,
-    @BsonProperty("outstanding_balance")
     val outstandingBalance: Double?,
-    @BsonProperty("monetary_correction")
     val monetaryCorrection: Double?,
     val amortization: Double?,
-    @BsonProperty("is_contribution")
     val isContribution: Boolean?
 ) {
     companion object {
         fun toDomain(installmentRequest: InstallmentRequest): Installments {
             return Installments(
-                id = null,
                 nroContrato = installmentRequest.nroContrato,
                 dueDate = installmentRequest.dueDate,
                 paid = installmentRequest.paid,
@@ -49,7 +38,6 @@ data class Installments(
     }
     fun toResponse(): InstallmentResponse {
         return InstallmentResponse(
-            id = id.toString(),
             nroContrato = nroContrato,
             dueDate = dueDate,
             paid = paid,
