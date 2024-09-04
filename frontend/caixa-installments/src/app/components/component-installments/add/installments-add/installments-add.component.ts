b6d2952d719ba190f3aa791b79a204501fe83d43
Ricardo <ricardohsmello@gmail.com>
2024-09-04 02:49:55 -0300

    adding  tesseras

--- frontend/caixa-installments/src/app/components/component-installments/add/installments-add/installments-add.component.ts
+++ frontend/caixa-installments/src/app/components/component-installments/add/installments-add/installments-add.component.ts
@@ -1,8 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
 import { Installment } from 'src/app/models/installments';
-
+import { InstallmentService } from 'src/app/services/service-installments/service/installment.service';
 
 @Component({
   selector: 'app-installments-add',
@@ -11,17 +10,27 @@ import { Installment } from 'src/app/models/installments';
 
 })
 export class InstallmentsAddComponent {
+
   constructor(
     public dialogRef: MatDialogRef<InstallmentsAddComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Installment
-   
-  ) {
-     
-  }
- 
+    @Inject(MAT_DIALOG_DATA) public data: Installment,
+    private installmentService: InstallmentService
+  ) {}
+
   onNoClick(): void {
     this.dialogRef.close();
   }
 
 
+  onFileSelected(event: Event): void {
+
+    
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) {
+      this.data.file = input.files[0];
+    }
+  }
+ 
+
+  
 }
