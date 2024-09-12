7d341c837efe50bcbe7cd368eba8cee7700ca23d
Ricardo <ricardohsmello@gmail.com>
2024-09-12 18:52:46 -0300

    refact

--- backend/src/main/kotlin/br/com/ricas/caixa/application/config/MongoConfig.kt
+++ backend/src/main/kotlin/br/com/ricas/caixa/application/config/MongoConfig.kt
@@ -3,6 +3,7 @@ package br.com.ricas.caixa.application.config
 import com.mongodb.kotlin.client.MongoClient
 import com.mongodb.client.MongoClients
 import com.mongodb.kotlin.client.MongoDatabase
+import org.springframework.ai.chat.client.ChatClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -25,4 +26,15 @@ class MongoConfig {
     fun mongoDatabase(mongoClient: MongoClient): MongoDatabase {
         return mongoClient.getDatabase(databaseName)
     }
+
+    @Bean
+    fun chatClient(chat: ChatClient.Builder): ChatClient {
+        return chat.build()
+    }
+
+    /*  private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }*/
 }
